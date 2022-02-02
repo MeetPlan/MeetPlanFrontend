a36e45b5e8d66b0744740f883df13ddd05ff0589
mytja <mytja@protonmail.com>
2022-02-02 19:54:13 +0100

    Add package.json

---
+++ src/App.svelte
@@ -0,0 +1,16 @@
+<script>
+	import { Router, Link, Route } from "svelte-routing";
+	import Samotestiranje from "./Samotestiranje.svelte";
+	import Home from "./Home.svelte";
+
+	export let url = "";
+</script>
+
+<main>
+	<Router url="{url}">
+		<div>
+			<Route path="/samotestiranje" component="{Samotestiranje}" />
+			<Route path="/"><Home /></Route>
+		</div>
+	</Router>
+</main>
