b86b37f942d8697bd322dc6249142ace2f00fc4d
mytja <mytja@protonmail.com>
2022-02-07 21:07:17 +0100

    API

--- src/constants.ts
+++ src/constants.ts
@@ -1,2 +1,2 @@
-export const baseurl: string = !isProduction ? "http://127.0.0.1:8000" : "http://backend";
+export const baseurl: string = !isProduction ? "http://127.0.0.1:8000" : "/api";
 console.log(baseurl)
