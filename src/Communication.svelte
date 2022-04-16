d7a8ea056a9c78ece6c03d548bcdf56629760944
mytja <mytja@protonmail.com>
2022-04-16 19:42:33 +0200

    Communication improvements: - Deleting & editing messages - Resolves https://github.com/MeetPlan/MeetPlanFrontend/issues/20 - Markdown parser in communication - Resolves https://github.com/MeetPlan/MeetPlanFrontend/issues/19

--- src/Communication.svelte
+++ src/Communication.svelte
@@ -8,14 +8,23 @@
 
     import Avatar from "svelte-avatar";
 
+    import List, { Group, Subheader, Item, Meta } from '@smui/list';
+    import Checkbox from '@smui/checkbox';
+
     import Dialog, { Title, Content, Actions } from '@smui/dialog';
     import FormField from '@smui/form-field';
     import Select, {Option} from "@smui/select";
 
+    import * as marked from 'marked';
+
     import Button, {Label} from "@smui/button";
     import Icon from '@smui/textfield/icon';
+    import jwt_decode, {JwtPayload} from "jwt-decode";
 
     export let id: string = "";
+
+    const token = localStorage.getItem("key");
+    const decoded = jwt_decode<JwtPayload>(token);
 
     function getCommunication() {
         fetch(`${baseurl}/communication/get/${id}`, {headers: {"Authorization": "Bearer " + localStorage.getItem("key")}})
@@ -25,13 +34,42 @@
             });
     }
 
-    let users = [];
+    function deleteMessage(messageId) {
+        fetch(`${baseurl}/message/get/${messageId}`, {headers: {"Authorization": "Bearer " + localStorage.getItem("key")}, method: "DELETE"})
+            .then((r) => r.json())
+            .then((r) => {
+                getCommunication();
+            });
+    }
+
+    function editMessage() {
+        let fd = new FormData();
+        fd.append("body", newMessageValue)
+        fetch(`${baseurl}/message/get/${messageEditingId}`, {headers: {"Authorization": "Bearer " + localStorage.getItem("key")}, method: "PATCH", body: fd})
+            .then((r) => r.json())
+            .then((r) => {
+                getCommunication();
+                messageEditingId = undefined;
+                newMessageValue = "";
+            });
+    }
+
+    let teachers = [];
+    let admins = [];
 
     function getUsers() {
         fetch(`${baseurl}/users/get`, {headers: {"Authorization": "Bearer " + localStorage.getItem("key")}})
             .then((response) => response.json())
             .then((json) => {
-                    users = json.data;
+                    let users = json.data;
+                    for (let i in users) {
+                        let user = users[i];
+                        if (user["Role"] === "admin") {
+                            admins = [...admins, user]
+                        } else if (user["Role"] === "teacher") {
+                            teachers = [...teachers, user]
+                        }
+                    }
                 },
             );
     }
@@ -39,6 +77,7 @@
     let comm;
 
     let newMessageValue = "";
+    let messageEditingId = undefined;
 
     let newCommunicationTitle = "";
     let usersAtCommunication = [];
@@ -46,6 +85,8 @@
     let user = "";
     let rerender = true;
     let rerenderDrawer = true;
+
+    let selected = [];
 
     function openDialog() {
         usersAtCommunication = [];
@@ -65,45 +106,47 @@
 <Dialog
         bind:open
         aria-labelledby="simple-title"
-        aria-describedby="simple-content"
+        aria-describedby="list-content"
 >
     <!-- Title cannot contain leading whitespace due to mdc-typography-baseline-top() -->
     <Title id="simple-title">
-        Nov pogovor
-    </Title>
-    <Content id="simple-content">
         <FormField>
             <Textfield
                     bind:value={newCommunicationTitle}
                     style="width: 100%;"
             />
         </FormField>
-        <br>
-        <FormField>
-            <Select bind:user label="Oseba" required>
-                {#each users as h}
-                    <Option value={h} on:click={() => {
-                        rerender = false;
-                        usersAtCommunication.push(h.ID)
-                        setTimeout(() => rerender = true, 100);
-                    }}>{h.Name}</Option>
+    </Title>
+    <Content id="list-content">
+        <Group>
+            <Subheader>Administratorji</Subheader>
+            <List checkList style="max-width: 600px; border-left: 1px solid var(--mdc-theme-text-hint-on-background, rgba(0, 0, 0, 0.1));">
+                {#each admins as h}
+                    <Item required>
+                        <Label>{h.Name}</Label>
+                        <Meta>
+                            <Checkbox bind:group={selected} value={h.ID} />
+                        </Meta>
+                    </Item>
                 {/each}
-            </Select>
-        </FormField>
-        <p/>
-        {#if rerender}
-            {#each users as user}
-                {#if usersAtCommunication.includes(user.ID)}
-                    {user.Name}
-                    <br>
-                {/if}
-            {/each}
-        {/if}
+            </List>
+            <Subheader>Učitelji</Subheader>
+            <List checkList style="max-width: 600px; border-left: 1px solid var(--mdc-theme-text-hint-on-background, rgba(0, 0, 0, 0.1));">
+                {#each teachers as h}
+                    <Item required>
+                        <Label>{h.Name}</Label>
+                        <Meta>
+                            <Checkbox bind:group={selected} value={h.ID} />
+                        </Meta>
+                    </Item>
+                {/each}
+            </List>
+        </Group>
     </Content>
     <Actions>
         <Button on:click={() => {
             let fd = new FormData();
-            fd.append("users", JSON.stringify(usersAtCommunication));
+            fd.append("users", JSON.stringify(selected));
             fd.append("title", newCommunicationTitle);
             fetch(`${baseurl}/communication/new`, {headers: {"Authorization": "Bearer " + localStorage.getItem("key")}, method: "POST", body: fd})
                 .then((r) => r.json())
@@ -130,8 +173,25 @@
                 <div style="white-space: nowrap;">
                     <Avatar name={message.UserName} style="display: inline-block;"/>
                     <h2 style="display: inline-block; text-align: center;">{message.UserName}</h2>
+                    {#if message.UserID === decoded["user_id"]}
+                        <Meta style="display: inline-block;">
+                            <Button on:click={() => deleteMessage(message.ID)}>
+                                <Icon class="material-icons">delete</Icon>
+                                <Label>Izbriši</Label>
+                            </Button>
+                        </Meta>
+                        <Meta style="display: inline-block;">
+                            <Button on:click={() => {
+                                messageEditingId = message.ID;
+                                newMessageValue = message.Body;
+                            }}>
+                                <Icon class="material-icons">edit</Icon>
+                                <Label>Uredi</Label>
+                            </Button>
+                        </Meta>
+                    {/if}
                 </div>
-                {message.Body}
+                {@html marked.marked(message.Body)}
             {/each}
             <hr>
             <Textfield
@@ -139,7 +199,12 @@
                     bind:value={newMessageValue}
                     style="width: 100%;"
             />
+            {#if newMessageValue !== ""}
+                <h2>Predogled</h2>
+                {@html marked.marked(newMessageValue)}
+            {/if}
             <p/>
+            {#if messageEditingId === undefined}
             <Button on:click={() => {
                 let fd = new FormData();
                 fd.append("body", newMessageValue)
@@ -152,6 +217,19 @@
                 <Icon class="material-icons">send</Icon>
                 <Label>Pošlji</Label>
             </Button>
+            {:else}
+                <Button on:click={() => editMessage()}>
+                    <Icon class="material-icons">done</Icon>
+                    <Label>Končaj z urejanjem</Label>
+                </Button>
+                <Button on:click={() => {
+                    messageEditingId = undefined;
+                    newMessageValue = "";
+                }}>
+                    <Icon class="material-icons">close</Icon>
+                    <Label>Končaj z urejanjem brez shranjevanja sprememb</Label>
+                </Button>
+            {/if}
         {/if}
     </main>
 </AppContent>
