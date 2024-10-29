9342dd585bb6d7d6a081c6185c2e27be36a6cd77
mytja <mitja@severkar.eu>
2024-10-29 03:27:49 +0100

    dodaj večino zakonsko zahtevanih podatkov o učencih in starših

--- src/User.svelte
+++ src/User.svelte
@@ -14,14 +14,23 @@
 
     let classes = [];
     let myClasses = [];
-    let classId = "";
-    let birthCertificateNumber = "";
-    let dateOfBirth = "";
-    let birthCountry = "";
-    let birthCity = "";
-    let name = "";
-    let email = "";
-    let role = "";
+    let classId: string = "";
+    let birthCertificateNumber: string = "";
+    let dateOfBirth: string = "";
+    let birthCountry: string = "";
+    let birthCity: string = "";
+    let name: string = "";
+    let surname: string = "";
+    let email: string = "";
+    let role: string = "";
+    let emso: string = "";
+    let citizenship: string = "";
+    let beforeAchievedEducation: string = "";
+    let permanentAddress: string = "";
+    let temporaryAddress: string = "";
+    let taxNumber: string = "";
+    let phoneNumber: string = "";
+    let gender: string = "";
     let students = [];
     let myStudents = [];
 
@@ -61,16 +70,34 @@
         name = r.data.Name;
         email = r.data.Email;
         role = r.data.Role;
+        surname = r.data.Surname;
+        emso = r.data.EMSO;
+        citizenship = r.data.Citizenship;
+        beforeAchievedEducation = r.data.BeforeAchievedEducation;
+        permanentAddress = r.data.PermanentAddress;
+        temporaryAddress = r.data.TemporaryAddress;
+        taxNumber = r.data.TaxNumber;
+        phoneNumber = r.data.PhoneNumber;
+        gender = r.data.Gender;
     }
 
     async function patchUser() {
         let fd = new FormData();
-        fd.append("birth_certificate_number", birthCertificateNumber !== undefined ? birthCertificateNumber : "");
-        fd.append("birthday", dateOfBirth !== undefined ? dateOfBirth : "");
-        fd.append("city_of_birth", birthCity !== undefined ? birthCity : "");
-        fd.append("country_of_birth", birthCountry !== undefined ? birthCountry : "");
-        fd.append("name", name !== undefined ? name : "");
-        fd.append("email", email !== undefined ? email : "");
+        fd.append("birth_certificate_number", birthCertificateNumber);
+        fd.append("birthday", dateOfBirth);
+        fd.append("city_of_birth", birthCity);
+        fd.append("country_of_birth", birthCountry);
+        fd.append("name", name);
+        fd.append("email", email);
+        fd.append("surname", surname);
+        fd.append("emso", emso);
+        fd.append("citizenship", citizenship);
+        fd.append("before_achieved_education", beforeAchievedEducation);
+        fd.append("permanent_address", permanentAddress);
+        fd.append("temporary_address", temporaryAddress);
+        fd.append("tax_number", taxNumber);
+        fd.append("phone_number", phoneNumber);
+        fd.append("gender", gender);
         await fetch(`${baseurl}/user/get/data/${id}`, {credentials: "include", method: "PATCH", body: fd});
         await getUserData();
     }
@@ -112,20 +139,52 @@
     <Textfield bind:value={birthCountry} label="Država rojstva" style="width: 100%;" required on:change={async () => await patchUser()}>
         <HelperText slot="helper">Vpišite državo rojstva - bodite zelo pozorni</HelperText>
     </Textfield>
+    <Textfield bind:value={dateOfBirth} type="date" required on:change={async () => await patchUser()}>
+        <Icon class="material-icons" slot="leadingIcon">event</Icon>
+        <HelperText slot="helper">Vpišite datum rojstva - bodite zelo pozorni</HelperText>
+    </Textfield>
+    <Textfield bind:value={emso} label="EMŠO" style="width: 100%;" required on:change={async () => await patchUser()}>
+        <HelperText slot="helper">Vpišite EMŠO - bodite zelo pozorni</HelperText>
+    </Textfield>
+    <Textfield bind:value={citizenship} label="Državljanstvo" style="width: 100%;" required on:change={async () => await patchUser()}>
+        <HelperText slot="helper">Vpišite državljanstvo (npr. slovensko) - bodite zelo pozorni</HelperText>
+    </Textfield>
+    <Textfield bind:value={beforeAchievedEducation} label="Predhodno pridobljena izobrazba" style="width: 100%;" on:change={async () => await patchUser()}>
+        <HelperText slot="helper">Vpišite predhodno pridobljeno izobrazbo (npr. OŠ Primer, 2013-2022)</HelperText>
+    </Textfield>
 {/if}
-<Textfield bind:value={dateOfBirth} type="date" required on:change={async () => await patchUser()}>
-    <Icon class="material-icons" slot="leadingIcon">event</Icon>
-    <HelperText slot="helper">Vpišite datum rojstva - bodite zelo pozorni (to se lahko uporabi za posebne funkcije ;-))</HelperText>
+{#if role === "student" || role === "parent"}
+    <Textfield bind:value={permanentAddress} label="Stalni naslov" style="width: 100%;" required on:change={async () => await patchUser()}>
+        <HelperText slot="helper">Vpišite stalni naslov - bodite zelo pozorni</HelperText>
+    </Textfield>
+    <Textfield bind:value={temporaryAddress} label="Začasni naslov" style="width: 100%;" required on:change={async () => await patchUser()}>
+        <HelperText slot="helper">Vpišite začasni naslov - bodite zelo pozorni</HelperText>
+    </Textfield>
+    <Textfield bind:value={taxNumber} label="Država rojstva" style="width: 100%;" on:change={async () => await patchUser()}>
+        <HelperText slot="helper">Vpišite davčno številko - bodite zelo pozorni</HelperText>
+    </Textfield>
+{/if}
+<Textfield bind:value={name} label="Ime" style="width: 100%;" required on:change={async () => await patchUser()}>
+    <HelperText slot="helper">Vpišite ime - bodite zelo pozorni</HelperText>
 </Textfield>
-<Textfield bind:value={name} label="Ime in priimek" style="width: 100%;" required on:change={async () => await patchUser()}>
-    <HelperText slot="helper">Vpišite ime in priimek - bodite zelo pozorni</HelperText>
+<Textfield bind:value={surname} label="Priimek" style="width: 100%;" required on:change={async () => await patchUser()}>
+    <HelperText slot="helper">Vpišite priimek - bodite zelo pozorni</HelperText>
 </Textfield>
 <Textfield bind:value={email} label="Elektronski naslov" style="width: 100%;" required on:change={async () => await patchUser()}>
     <HelperText slot="helper">Vpišite elektronski naslov - bodite zelo pozorni</HelperText>
 </Textfield>
+<Textfield bind:value={phoneNumber} label="Telefonska številka" style="width: 100%;" required on:change={async () => await patchUser()}>
+    <HelperText slot="helper">Vpišite telefonsko številko - bodite zelo pozorni</HelperText>
+</Textfield>
+<Select bind:value={gender} label="Izberite spol" variant="outlined" on:MDCSelect:change={async () => await patchUser()}>
+    <Option value=""/>
+    <Option value="male">Moški</Option>
+    <Option value="female">Ženski</Option>
+</Select>
+
 {#if role === "student"}
     <h2>Dodaj učenca v razred:</h2>
-    <Select bind:classId label="Izberite razred" variant="outlined">
+    <Select bind:value={classId} label="Izberite razred" variant="outlined">
         <Option value=""/>
         {#each classes as c}
             <Option on:click={async () => {await assignToClass(c["ID"])}} value={c["ID"]}>{c["Name"]}</Option>
